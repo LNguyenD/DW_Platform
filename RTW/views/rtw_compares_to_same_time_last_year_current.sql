3337d8523fb9623b892be1d8a2b70a1406cafafa
LNguyenD <L.NguyenD@ASWVNWKS084.aswigsyd.int>
2016-01-19 13:25:42 +0700

    no message

--- RTW/views/rtw_compares_to_same_time_last_year_current.sql
+++ RTW/views/rtw_compares_to_same_time_last_year_current.sql
@@ -33,46 +33,46 @@ AS
 			select 104 as Measure_months) as measure_months		
 			cross join
 			(
-				select distinct Agency_Name as EmployerSize_Group, [Type]='agency', [System]
+				select distinct Agency_Name as Value, [Type]='agency', [System]
 				from views.rtw_view uv 
 				where  uv.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11)
 				
 				union
 				
-				select distinct rtrim(EMPL_SIZE) as EmployerSize_Group, [Type]='employer_size', [System]
+				select distinct rtrim(EMPL_SIZE) as Value, [Type]='employer_size', [System]
 				from views.rtw_view uv 
 				where  uv.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11)
 				
 				union
-				select distinct rtrim([Account_Manager]) as EmployerSize_Group, [Type]='account_manager', [System]
+				select distinct rtrim([Account_Manager]) as Value, [Type]='account_manager', [System]
 				from views.rtw_view uv 
 				where  uv.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11)
 				
 				union
-				select distinct rtrim(Portfolio) as EmployerSize_Group, [Type]='portfolio', [System]
+				select distinct rtrim(Portfolio) as Value, [Type]='portfolio', [System]
 				from views.rtw_view uv 
 				where  uv.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11)		
 					
 				union
-				select distinct [Group] as EmployerSize_Group, [Type]='portfolio', [System]
+				select distinct [Group] as Value, [Type]='portfolio', [System]
 				from views.rtw_view uv 
 				where uv.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11)				   				
 				
 				union all
 								
-				select distinct [Grouping] as EmployerSize_Group, [Type] = 'grouping', [System]
+				select distinct [Grouping] as Value, [Type] = 'grouping', [System]
 				from views.rtw_view uv
 				where uv.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11) and [Grouping] <> ''
 					
 				union all
 				
-				select distinct [System] as EmployerSize_Group, [Type] = '', [System]
+				select distinct [System] as Value, [Type] = '', [System]
 				from views.rtw_view uv
 				where uv.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11)
@@ -90,15 +90,15 @@ AS
 						 when DATEDIFF(MM, Remuneration_Start, Remuneration_End) = 11
 							then 12
 					end,
-					[Type], rtw.[System], Measure_months = Measure, EmployerSize_Group,					
+					[Type], rtw.[System], Measure_months = Measure, Value,					
 					SUM(LT) as LT,
 					SUM(WGT) as WGT,
 					AVGDURN = SUM(LT) / NULLIF(SUM(WGT),0)		
-				    ,[Target] = sum(LT)/nullif(sum(WGT),0)*100/nullif(udfs.rtw_gettargetandbase(rtw.[System], Remuneration_End, 'target', [Type], EmployerSize_Group, NULL, Measure),0)
+				    ,[Target] = sum(LT)/nullif(sum(WGT),0)*100/nullif(udfs.rtw_gettargetandbase(rtw.[System], Remuneration_End, 'target', [Type], Value, NULL, Measure),0)
 				    
 			from    views.rtw_view rtw
 					inner join values_by_type val
-						on val.EmployerSize_Group = case when val.[Type] = 'agency'
+						on val.Value = case when val.[Type] = 'agency'
 												then rtw.Agency_Name
 											when val.[Type] = 'group'
 												then rtw.[Group]
@@ -114,7 +114,7 @@ AS
 										end AND val.[System] = rtw.[System]
 			where	rtw.Remuneration_End = (SELECT max(Remuneration_End) FROM  views.rtw_view)
 					and  DATEDIFF(MM, Remuneration_Start, Remuneration_End) in (0,2,5,11)
-			group by [Type], rtw.[System], Measure, EmployerSize_Group, Remuneration_Start, Remuneration_End
+			group by [Type], rtw.[System], Measure, Value, Remuneration_Start, Remuneration_End
 
 GO
 
