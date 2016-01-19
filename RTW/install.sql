cd88e03ccb4f0c9429ab0a895426a1955fe6bd40
unknown <d.vo@ASWVNWKS062.aswigsyd.int>
2016-01-19 11:42:04 +0700

    Cleaned up coding

--- RTW/install.sql
+++ RTW/install.sql
@@ -10,15 +10,8 @@ SET NOCOUNT ON
 :r $(path)\views\rtw_view.sql
 :r $(path)\views\rtw_addtargetandbase.sql
 
-:r $(path)\udfs\emi_rtw_gettargetandbase.sql
-:r $(path)\udfs\tmf_rtw_gettargetandbase.sql
-:r $(path)\udfs\hem_rtw_gettargetandbase.sql
+:r $(path)\udfs\rtw_gettargetandbase.sql
 :r $(path)\udfs\tmf_rtw_agency_group_compares_to_same_time_last_year_current_add_missing_group.sql
 
-:r $(path)\views\emi_rtw_agency_group_compares_to_same_time_last_year_current.sql
-:r $(path)\views\tmf_rtw_agency_group_compares_to_same_time_last_year_current.sql
-:r $(path)\views\hem_rtw_agency_group_compares_to_same_time_last_year_current.sql
-
-:r $(path)\views\emi_rtw_agency_group_rolling_month_12.sql
-:r $(path)\views\tmf_rtw_agency_group_rolling_month_12.sql
-:r $(path)\views\hem_rtw_agency_group_rolling_month_12.sql
+:r $(path)\views\rtw_compares_to_same_time_last_year_current.sql
+:r $(path)\views\rtw_rolling_month_12.sql
